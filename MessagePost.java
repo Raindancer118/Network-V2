85ec970ddda1f7ff0f1378fa0b9564c370764c33
Raindaincer118 <tom560stieh@gmail.com>
2024-09-24 12:21:10 +0200

    Network-V2 nach Bearbeitung, aber für den drecks Test optimiert

--- MessagePost.java
+++ MessagePost.java
@@ -25,6 +25,30 @@ public class MessagePost extends Post {
         comments = new ArrayList<>();
     }
 
+    /**
+     * Record one more 'Like' indication from a user.
+     */
+    public void like()   {
+        likes++;
+    }
+
+    /**
+     * Record that a user has withdrawn his/her 'Like' vote.
+     */
+    public void unlike()    {
+        if (likes > 0) {
+            likes--;
+        }
+    }
+
+    /**
+     * Add a comment to this post.
+     * 
+     * @param text  The new comment to add.
+     */
+    public void addComment(String text)    {
+        comments.add(text);
+    }
 
     /**
      * Return the text of this post.
@@ -33,6 +57,15 @@ public class MessagePost extends Post {
      */
     public String getText()    {
         return message;
+    }
+
+    /**
+     * Return the time of creation of this post.
+     * 
+     * @return The post's creation time, as a system time value.
+     */
+    public long getTimeStamp()    {
+        return timestamp;
     }
 
     /**
@@ -58,5 +91,25 @@ public class MessagePost extends Post {
             System.out.println("   " + comments.size() + " comment(s). Click here to view.");
         }
     }
-
+    
+    /**
+     * Create a string describing a time point in the past in terms 
+     * relative to current time, such as "30 seconds ago" or "7 minutes ago".
+     * Currently, only seconds and minutes are used for the string.
+     * 
+     * @param time  The time value to convert (in system milliseconds)
+     * @return      A relative time string for the given time
+     */
+    
+    private String timeString(long time)    {
+        long current = System.currentTimeMillis();
+        long pastMillis = current - time;      // time passed in milliseconds
+        long seconds = pastMillis/1000;
+        long minutes = seconds/60;
+        if(minutes > 0) {
+            return minutes + " minutes ago";
+        } else {
+            return seconds + " seconds ago";
+        }
+    }
 }
